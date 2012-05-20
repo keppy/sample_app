39094e382ed9c5d39019bc961630bcc8082520ec
keppy <feralmode@gmail.com>
2012-05-19 17:57:16 -0700

    Refactored tests

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,12 @@
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
+include ApplicationHelper
+
+# The following code is old. Testing has been restructured through the use of ApplicationHelper.
+
+#def full_title(page_title)
+#  base_title = "Ruby on Rails Tutorial Sample App"
+#  if page_title.empty?
+#    base_title
+#  else
+#    "#{base_title} | #{page_title}"
+#  end
+#end
