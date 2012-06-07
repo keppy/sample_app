f7aab95d9c2f0f796f5a9965a2ab17f7bed36926
keppy <feralmode@gmail.com>
2012-06-07 00:19:38 -0700

    Refactor and add cucumer

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,5 +1,17 @@
 include ApplicationHelper
 
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
 # The following code is old. Testing has been restructured through the use of ApplicationHelper.
 
 #def full_title(page_title)
